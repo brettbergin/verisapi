7904ba94aa11e73d949be44717b0eb8493c15297
lethal <lethal@lethals-MacBook-Pro.local>
2016-05-03 01:56:25 -0700

    Fixed run.py

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
-# ecommerce_victim_id = 454111
 
 from api import app
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000, debug=True)
+    app.run(host='127.0.0.1', port=8000)
